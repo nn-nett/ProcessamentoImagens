6ddfc47bd93e9a6b4475a7fac001997dee277d2d
nn-nett <nn.nett@gmail.com>
2020-09-15 22:21:37 -0300

    alteracoes

--- images.py
+++ images.py
@@ -1,7 +1,10 @@
 from PIL import Image, ImageFilter
+from os import path
+
+print(os.path.relpath)
 
 img = Image.open('C:\\Users\\rpgni\\PycharmProjects\\ProcessamentoImagens\\venv\\Pokedex\\pikachu.jpg')
-#filtered_img = img.filter(ImageFilter.SHARPEN)
+
 filtered_img = img.convert('L')
 filtered_img.save("grey.png", 'png')
 
